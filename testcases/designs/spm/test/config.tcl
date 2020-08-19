a8c2ae32f5f9d903b0353bcf4c43d35adfcfec3a
agorararmard <aagouhar@efabless.com>
2020-08-19 23:09:44 +0200

    first test case

---
+++ testcases/designs/spm/test/config.tcl
@@ -0,0 +1,12 @@
+# General config
+set ::env(PDK) sky130A
+set ::env(STD_CELL_LIBRARY) sky130_fd_sc_hd
+set ::env(PDK_ROOT) $::env(PDK_ROOT)
+set ::env(CELL_PAD) 8
+set ::env(MERGED_LEF) $::env(test_dir)/merged.lef
+set ::env(MERGED_LEF_UNPADDED) $::env(test_dir)/merged_unpadded.lef
+set ::env(TECH_LEF) $::env(PDK_ROOT)/sky130A/libs.ref/sky130_fd_sc_hd/techlef/sky130_fd_sc_hd.tlef
+set ::env(CURRENT_DEF) $::env(test_dir)/spm.def
+set ::env(MAGIC_MAGICRC) $::env(test_dir)/.magicrc
+
+
