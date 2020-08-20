4dde757457bf7715c6e510ff96821671f9eef9c9
agorararmard <aagouhar@efabless.com>
2020-08-20 13:08:43 +0000

    attempt at fix pdk build

--- travisCI/travisBuild.sh
+++ travisCI/travisBuild.sh
@@ -16,6 +16,19 @@ export PDK_ROOT=$(pwd)/pdks
 export RUN_ROOT=$(pwd)
 echo $PDK_ROOT
 echo $RUN_ROOT
+export STD_CELL_LIBRARY=sky130_fd_sc_hd
+cd  $PDK_ROOT
+rm -rf skywater-pdk
+git clone https://github.com/google/skywater-pdk.git skywater-pdk
+cd skywater-pdk
+git checkout -qf 3f310bcc264df0194b9f7e65b83c59759bb27480
+git submodule update --init libraries/$STD_CELL_LIBRARY/latest
+make $STD_CELL_LIBRARY
+cd $PDK_ROOT
+rm -rf open_pdks
+git clone https://github.com/RTimothyEdwards/open_pdks.git open_pdks
+cd open_pdks
+git checkout -qf 60b4f62aabff2e4fd9df194b6db59e61a2bd2472
 docker run -it -v $RUN_ROOT:/magic_root -v $PDK_ROOT:$PDK_ROOT -e PDK_ROOT=$PDK_ROOT -u $(id -u $USER):$(id -g $USER) magic:latest  bash -c "sh ./travisCI/buildPDK.sh"
 echo "done installing"
 cd $RUN_ROOT
